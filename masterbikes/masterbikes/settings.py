5ca58e3534098c1b42d4103c94df2393fc2453b2
s1s1kosiss <benjamincontreras2120@gmail.com>
2025-05-03 17:14:54 -0400

    design: Configuración del proyecto
    - Actualización de settings.py con apps y configuraciones
    - Configuración de URLs principales
    - Actualización de requirements.txt con nuevas dependencias

--- masterbikes/masterbikes/settings.py
+++ masterbikes/masterbikes/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Nuestras aplicaciones
+    'widget_tweaks',
     'usuarios',
     'productos',
     'arriendos',
