c36066e00f5416a4a5edf9559ed94dd3e0145ce5
s1s1kosiss <benjamincontreras2120@gmail.com>
2025-05-03 18:30:26 -0400

    chore: cambio de configuración a SQLite para desarrollo local

--- masterbikes/masterbikes/settings.py
+++ masterbikes/masterbikes/settings.py
@@ -84,12 +84,8 @@ WSGI_APPLICATION = 'masterbikes.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'masterbikes',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
