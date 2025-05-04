5b12ec2677292b36ddd126a773872140088dd93d
s1s1kosiss <benjamincontreras2120@gmail.com>
2025-05-04 18:24:09 -0400

    feat: implementa recuperación de contraseña y corrige enlace en login (namespace usuarios)

--- masterbikes/usuarios/urls.py
+++ masterbikes/usuarios/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 app_name = 'usuarios'
 
@@ -13,4 +14,8 @@ urlpatterns = [
     path('cliente/perfil/', views.perfil_cliente, name='perfil_cliente'),
     path('cliente/arriendos/', views.ArriendosClienteListView.as_view(), name='arriendos_cliente'),
     path('cliente/reparaciones/', views.ReparacionesClienteListView.as_view(), name='reparaciones_cliente'),
+    path('password-reset/', auth_views.PasswordResetView.as_view(template_name='usuarios/password_reset.html'), name='password_reset'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='usuarios/password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='usuarios/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='usuarios/password_reset_complete.html'), name='password_reset_complete'),
 ] 
